8b8c90b58d8d1810d824bdad636e4d76975cfa08
EpicVS <sharmav39@gmail.com>
2015-09-26 10:45:17 -0400

    added stuff

--- app/src/main/java/hackcmu/lasertag/GameFragment.java
+++ app/src/main/java/hackcmu/lasertag/GameFragment.java
@@ -1,11 +1,15 @@
 package hackcmu.lasertag;
 
 
-        import android.app.Activity;
+import android.app.Activity;
 import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
-import android.widget.FrameLayout;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.FrameLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.vision.CameraSource;
@@ -17,34 +21,66 @@ import java.util.List;
 import static java.lang.System.loadLibrary;
 
 
-/**
- * Created by Nick on 9/26/2015.
- */
 public class GameFragment extends Fragment {
 
-        static {
-            loadLibrary("scanditsdk-android-4.8.1");
-        }
-        private CameraSource mSource;
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            ScanditView scanditView = new ScanditView(this.getActivity()) { //TODO: CHANGE
-                public void onScan(ScanditSDKScanSession scanditSDKScanSession, Activity a) {
-                    List<ScanditSDKCode> codes = scanditSDKScanSession.getAllCodes();
-                    if (codes.size() > 0) {
-                        Context context = a.getApplicationContext();
-                        ScanditSDKCode code = codes.get(0);
-                        Toast toast = Toast.makeText(context, "You have shot " + code.getData() + "!", Toast.LENGTH_SHORT);
-                        toast.show();
-                        this.pauseScanning();
-                    }
+    static {
+        loadLibrary("scanditsdk-android-4.8.1");
+    }
+    private CameraSource mSource;
+
+    private TextView redScore, blueScore;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    public static GameFragment newInstance() {
+        GameFragment fragment = new GameFragment();
+
+        return fragment;
+    }
+
+    public GameFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        ScanditView scanditView = new ScanditView(this.getActivity()) { //TODO: CHANGE
+            public void onScan(ScanditSDKScanSession scanditSDKScanSession, Activity a) {
+                List<ScanditSDKCode> codes = scanditSDKScanSession.getAllCodes();
+                if (codes.size() > 0) {
+                    Context context = a.getApplicationContext();
+                    ScanditSDKCode code = codes.get(0);
+                    onShoot(code.getData());
+
+                    this.pauseScanning();
                 }
-            };
-            getActivity().setContentView(R.layout.activity_join_game); //TODO: CHANGE
-            FrameLayout frameLayout = (FrameLayout) getActivity().findViewById(R.id.placeholder); //TODO: CHANGE
-            frameLayout.addView(scanditView);
-        }
+            }
+        };
+        ((FrameLayout) getActivity().findViewById(R.id.placeholder)).addView(scanditView);
+
+        redScore = (TextView) getActivity().findViewById(R.id.red_team_score);
+        blueScore= (TextView) getActivity().findViewById(R.id.blue_team_score);
+
+        return inflater.inflate(R.layout.fragment_game, container, false);
+    }
+
+    public void updateScore(int red, int blue) {
+        redScore.setText(red+"");
+        blueScore.setText(blue+"");
+    }
+
+    public void onShoot(String barcode){
+        Toast toast = Toast.makeText(getActivity().getApplicationContext(),
+                "You have shot " + barcode + "!", Toast.LENGTH_SHORT);
+        toast.show();
+        // TODO: implement this
+    }
 
 
 }
