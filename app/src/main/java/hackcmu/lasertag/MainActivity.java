e63be23ab8f2dcfa4a7ff67c3cbf0827dcab0231
vrama628 <vrama628@gmail.com>
2015-09-25 22:09:29 -0400

    Revert "updated stuff"
    This reverts commit 56828dc6d86956d90b59e05942bfedb436dac2c0.

--- app/src/main/java/hackcmu/lasertag/MainActivity.java
+++ app/src/main/java/hackcmu/lasertag/MainActivity.java
@@ -1,12 +1,9 @@
 package hackcmu.lasertag;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -14,24 +11,6 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final Button hostGameBtn = (Button) findViewById(R.id.host_game_btn);
-        hostGameBtn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent myIntent = new Intent(MainActivity.this, HostGameActivity.class);
-                MainActivity.this.startActivity(myIntent);
-            }
-        });
-
-        final Button joinGameBtn = (Button) findViewById(R.id.join_game_btn);
-        joinGameBtn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent myIntent = new Intent(MainActivity.this, JoinGameActivity.class);
-                MainActivity.this.startActivity(myIntent);
-            }
-        });
-
-
     }
 
     @Override
